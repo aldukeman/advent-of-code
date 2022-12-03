ff607e1832b57e1a60d9778f36dab23b278a0844
Anton Dukeman <anton.dukeman@gmail.com>
2022-12-03 12:58:43 -0600

    More cascading

--- 2022/03/Part2.swift
+++ 2022/03/Part2.swift
@@ -6,11 +6,9 @@ let lowerAValue = "a".unicodeScalars.first!.value
 let allValues = Set<UInt32>(1...52)
 var badges = [UInt32]()
 while let firstLine = readLine() {
-  let secondLine = readLine()
-  let thirdLine = readLine()
-  let lines = [firstLine, secondLine, thirdLine].compactMap { $0 }
-
-  let badge = lines.map { line in
+  let badge = [firstLine, readLine(), readLine()]
+    .compactMap { $0 }
+    .map { line in
       line.unicodeScalars
         .map { $0.value }
         .map { $0 >= lowerAValue ? $0 - lowerAValue : $0 - capAValue + 26 }
